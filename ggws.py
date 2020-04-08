2c8de2ef1f68fbaaeb1375aaf14d2d640f28b916
Sean Robertson <sdrobert@cs.toronto.edu>
2020-04-08 18:02:32 -0400

    Completed preamble command

--- ggws.py
+++ ggws.py
@@ -21,6 +21,11 @@
 import os
 import sys
 import argparse
+import locale
+
+from collections import Counter
+
+from common import mkdir
 
 __author__ = "Sean Robertson"
 __email__ = "sdrobert@cs.toronto.edu"
@@ -28,8 +33,54 @@ __license__ = "Apache 2.0"
 __copyright__ = "Copyright 2020 Sean Robertson"
 
 
+locale.setlocale(locale.LC_ALL, 'C')
+
+
 def preamble(options):
-    pass
+
+    dir_ = os.path.join(options.data_root, 'local', 'data')
+    src = os.path.join(options.ggws_root, 'org_data')
+    word2freq_txt = os.path.join(dir_, 'word2freq.txt')
+
+    if not os.path.exists(src):
+        raise ValueError(
+            '"{}" does not exist. Are you sure "{}" points to the UniLM '
+            'version of the Gigaword Summarization Corpus?'.format(
+                src, options.ggws_root))
+
+    mkdir(dir_)
+
+    word2freq = Counter()
+
+    for fn_nosuf in ('train', 'dev', 'test'):
+        is_train = fn_nosuf == 'train'
+        for suffix in ('.src.txt', '.tgt.txt'):
+            with \
+                    open(
+                        os.path.join(src, fn_nosuf + suffix),
+                        encoding='utf-8') as in_, \
+                    open(os.path.join(dir_, fn_nosuf + suffix), 'w') as out_:
+                for line in in_:
+                    # the non-breaking space sometimes comes up instead of a
+                    # space. We also replace the underscore with its html code
+                    # so that it doesn't muck with our use of underscore in
+                    # subwords (the database hasn't been entirely sanitized of
+                    # these anyway)
+                    line = line.replace(u'\u00A0', ' ').replace('_', '&#95;')
+                    # replace 'UNK' in the test set with '<unk>' to be
+                    # consistent with the training set. I prefer '<unk>'
+                    # because it's quite clearly a control character
+                    tokens = line.strip().split(' ')
+                    if is_train:
+                        word2freq.update(tokens)
+                    else:
+                        tokens = ['<unk>' if x == 'UNK' else x for x in tokens]
+                    out_.write(' '.join(tokens))
+                    out_.write('\n')
+
+    with open(word2freq_txt, 'w') as file_:
+        for word, freq in sorted(word2freq.items()):
+            file_.write('{} {}\n'.format(word, freq))
 
 
 def build_preamble_parser(subparsers):
